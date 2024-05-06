4106c8b974bc3c1dd79feccbff12c1828d419a12
Callum Leslie <git@cleslie.uk>
2024-05-06 17:07:19 +0100

    hyprland configured properly woohooo

--- home/programs/hypr/default.nix
+++ home/programs/hypr/default.nix
@@ -4,174 +4,161 @@
     systemd.enable = true;
     xwayland.enable = true;
     # TODO: move to nix config over text
-    extraConfig = ''
-      monitor=,preferred,auto,1
+    settings = {
+      "monitor" = ",prefered,auto,1";
 
-      general {
-          gaps_in = 0
-          gaps_out = 0
-          border_size = 1
-          col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
-          col.inactive_border = rgba(595959aa)
+      general = {
+        gaps_in = 0;
+        gaps_out = 0;
+        border_size = 1;
+        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.inactive_border" = "rgba(595959aa";
 
-          layout = dwindle
-      }
+      };
 
-      decoration {
-          drop_shadow = yes
-          shadow_range = 4
-          shadow_render_power = 3
-          col.shadow = rgba(1a1a1aee)
-      }
+      decoration = {
+        drop_shadow = true;
+        shadow_range = 4;
+        shadow_render_power = 3;
+        "col.shadow" = "rgba(1a1a1aee)";
+      };
 
-      animations {
-          enabled = yes
+      animations = {
+        enabled = true;
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+        animation = [
+          "windows, 1, 1, default, popin"
+          "fade, 1, 7, default"
+          "workspaces, 1, 1, default, fade"
+        ];
+      };
 
-          bezier = myBezier, 0.05, 0.9, 0.1, 1.05
+      dwindle = {
+        pseudotile = true;
+        preserve_split = true;
+        no_gaps_when_only = 1;
+      };
 
-          animation = windows, 1, 1, default, popin
-          animation = fade, 1, 7, default
-          animation = workspaces, 1, 1, default, fade
-      }
+      master = {
+        new_is_master = true;
+        no_gaps_when_only = 1;
+      };
 
-      dwindle {
-          pseudotile = yes
-          preserve_split = yes
-          no_gaps_when_only = 1
-      }
+      gestures.workspace_swipe = false;
 
-      master {
-          new_is_master = true
-          no_gaps_when_only = 1
-      }
+      misc = {
+        enable_swallow = true;
+        swallow_regex = "Alacritty";
+        disable_hyprland_logo = true;
+        disable_splash_rendering = true;
+      };
 
-      gestures {
-          workspace_swipe = false
-      }
+      input = {
+        kb_layout = "gb,us";
+        kb_variant = ",workman";
+        kb_model = "";
+        kb_options = "ctrl:nocaps, grp:alt_shift_toggle";
+        kb_rules = "";
 
-      misc {
-        enable_swallow = true
-        swallow_regex = Alacritty
-      }
+        follow_mouse = true;
 
-      input {
-          kb_layout = gb,us
-          kb_variant = ,workman
-          kb_model =
-          kb_options = ctrl:nocaps, grp:alt_shift_toggle
-          kb_rules =
+        touchpad = {
+          natural_scroll = true;
+          scroll_factor = 0.3;
+        };
 
-          follow_mouse = 1
+        sensitivity = 0.5;
+        accel_profile = "flat";
+        scroll_method = "2fg";
+      };
 
-          touchpad {
-              natural_scroll = yes
-              scroll_factor = 0.3
-          }
+      "$mainMod" = "SUPER";
 
-          sensitivity = 0.5
-          accel_profile = flat
-          scroll_method = 2fg
-      }
+      bind = [
+        "$mainMod, q, killactive"
+        "$mainMod SHIFT, q, exit"
+        "$mainMod, F, fullscreen"
+        "$mainMod SHIFT, f, togglefloating"
+        "$mainMod, d, exec, rofi -show drun"
+        "$mainMod, w, exec, rofi -show window"
+        "$mainMod, p, exec, rofi-rbw --no-folder"
+        "$mainMod, s, togglesplit"
+        "$mainMod SHIFT, r, exec, hyprctl reload"
+        "$mainMod, return, exec, alacritty -e tmux new -A -s main"
+        "$mainMod SHIFT, return, exec, [float; pin] alacritty -e tmux new -A -s main"
 
-      misc {
-          disable_hyprland_logo = true;
-          disable_splash_rendering = true;  
-      }
+        "$mainMod, b, workspace, name:web"
+        "$mainMod, n, workspace, name:chat"
+        "$mainMod, m, workspace, name:media"
+        "$mainMod, v, workspace, name:mail"
 
-      $mainMod = SUPER
+        "$mainMod SHIFT, b, movetoworkspace, name:web"
+        "$mainMod SHIFT, n, movetoworkspace, name:chat"
+        "$mainMod SHIFT, m, movetoworkspace, name:media"
+        "$mainMod SHIFT, v, movetoworkspace, name:mail"
 
-      # Utility Binds
-      bind = $mainMod, q, killactive, 
-      bind = $mainMod SHIFT, q, exit, 
-      bind = $mainMod, F, fullscreen, 
-      bind = $mainMod SHIFT, f, togglefloating
-      bind = $mainMod, d, exec, rofi -show drun
-      bind = $mainMod, w, exec, rofi -show window
-      bind = $mainMod, p, exec, rofi-rbw --no-folder
-      bind = $mainMod, s, togglesplit, # dwindle
-      bind = $mainMode SHIFT, r, exec, hyprctl reload
+        "$mainMod CTRL, b, moveworkspacetomonitor, name:web current"
+        "$mainMod CTRL, n, moveworkspacetomonitor, name:chat current"
+        "$mainMod CTRL, m, moveworkspacetomonitor, name:media current"
+        "$mainMod CTRL, v, moveworkspacetomonitor, name:mail current"
+        "$mainMod CTRL, b, workspace, name:web"
+        "$mainMod CTRL, n, workspace, name:chat"
+        "$mainMod CTRL, m, workspace, name:media"
+        "$mainMod CTRL, v, workspace, name:mail"
 
-      # Program Binds
-      bind = $mainMod, return, exec, alacritty -e tmux new -A -s main
-      bind = $mainMod SHIFT, return, exec, [float; pin] alacritty -e tmux new -A -s main
-      bind = $mainMod SHIFT, b, exec, firefox
-      bind = $mainMod, b, workspace, name:web
-      bind = $mainMod, n, workspace, name:chat
-      bind = $mainMod, m, workspace, name:media
-      bind = $mainMod, v, workspace, name:mail
+        "$mainMod, h, movefocus, l"
+        "$mainMod, l, movefocus, r"
+        "$mainMod, k, movefocus, u"
+        "$mainMod, j, movefocus, d"
+        "$mainMod SHIFT, h, movewindow, l"
+        "$mainMod SHIFT, l, movewindow, r"
+        "$mainMod SHIFT, k, movewindow, u"
+        "$mainMod SHIFT, j, movewindow, d"
+        ''SHIFT, Print, exec, grim -g "$(slurp)" - | wl-copy''
+        ", Print, exec, grim - | wl-copy"
+      ] ++ (builtins.concatLists (builtins.genList (x:
+        let ws = let c = (x + 1) / 10; in builtins.toString (x + 1 - (c * 10));
+        in [
+          "$mainMod, ${ws}, workspace, ${toString (x + 1)}"
+          "$mainMod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+          "$mainMod CTRL, ${ws}, moveworkspacetomonitor, ${
+            toString (x + 1)
+          } current"
+          "$mainMod CTRL, ${ws}, workspace, ${toString (x + 1)}"
+        ]) 10));
 
-      # Traverse Windows
-      bind = $mainMod, h, movefocus, l
-      bind = $mainMod, l, movefocus, r
-      bind = $mainMod, k, movefocus, u
-      bind = $mainMod, j, movefocus, d
+      workspace = [
+        "name:web, on-created-empty: firefox"
+        "name:chat, on-created-empty: discord"
+        "name:media, on-created-empty: spotify"
+        "name:mail, on-created-empty: betterbird"
+      ];
 
-      bind = $mainMod SHIFT, h, movewindow, l
-      bind = $mainMod SHIFT, l, movewindow, r
-      bind = $mainMod SHIFT, k, movewindow, u
-      bind = $mainMod SHIFT, j, movewindow, d
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
 
-      # Custom Workspaces
+      bindle = [
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86AudioMute , exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ", XF86AudioMicMute , exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ", XF86AudioPlay , exec, playerctl play-pause"
+        ", XF86AudioPause , exec, playerctl play-pause"
+        ", XF86AudioNext , exec, playerctl next"
+        ", XF86AudioPrev , exec, playerctl previous"
+        ", XF86MonBrightnessUp, exec, brightnessctl -c backlight set +5%"
+        ", XF86MonBrightnessDown, exec, brightnessctl -c backlight set 5%-"
+      ];
 
-      workspace = name:web, on-created-empty: firefox
-      workspace = name:chat, on-created-empty: discord
-      workspace = name:media, on-created-empty: spotify
-      workspace = name:mail, on-created-empty: betterbird
+      exec = [
+        "pkill wpaperd & sleep 0.5 && wpaperd"
+        "pkill waybar & sleep 0.5 && waybar"
+        "pkill mako & sleep 0.5 && mako"
+      ];
 
-
-      # Traverse Workspaces
-      bind = $mainMod, 1, workspace, 1
-      bind = $mainMod, 2, workspace, 2
-      bind = $mainMod, 3, workspace, 3
-      bind = $mainMod, 4, workspace, 4
-      bind = $mainMod, 5, workspace, 5
-      bind = $mainMod, 6, workspace, 6
-      bind = $mainMod, 7, workspace, 7
-      bind = $mainMod, 8, workspace, 8
-      bind = $mainMod, 9, workspace, 9
-      bind = $mainMod, 0, workspace, 10
-
-      bind = $mainMod SHIFT, 1, movetoworkspace, 1
-      bind = $mainMod SHIFT, 2, movetoworkspace, 2
-      bind = $mainMod SHIFT, 3, movetoworkspace, 3
-      bind = $mainMod SHIFT, 4, movetoworkspace, 4
-      bind = $mainMod SHIFT, 5, movetoworkspace, 5
-      bind = $mainMod SHIFT, 6, movetoworkspace, 6
-      bind = $mainMod SHIFT, 7, movetoworkspace, 7
-      bind = $mainMod SHIFT, 8, movetoworkspace, 8
-      bind = $mainMod SHIFT, 9, movetoworkspace, 9
-      bind = $mainMod SHIFT, 0, movetoworkspace, 10
-
-      # Resize
-      bindm = $mainMod, mouse:272, movewindow
-      bindm = $mainMod, mouse:273, resizewindow
-
-      # XF86 Binds
-      bindle = , XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
-      bindle = , XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
-      bindle = , XF86AudioMute , exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-
-      bindle = , XF86AudioMicMute , exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
-
-      bindle = , XF86AudioPlay , exec, playerctl play-pause
-      bindle = , XF86AudioPause , exec, playerctl play-pause
-      bindle = , XF86AudioNext , exec, playerctl next
-      bindle = , XF86AudioPrev , exec, playerctl previous
-
-      bindle = , XF86MonBrightnessUp, exec, brightnessctl -c backlight set +5%
-      bindle = , XF86MonBrightnessDown, exec, brightnessctl -c backlight set 5%-
-
-      # Print Screen
-      bind = SHIFT, Print, exec, grim -g "$(slurp)" - | wl-copy
-      bind = , Print, exec, grim - | wl-copy
-
-      exec = pkill wpaperd & sleep 0.5 && wpaperd
-      exec = pkill waybar & sleep 0.5 && waybar
-      exec = pkill mako & sleep 0.5 && mako
-      exec = blueman-tray
-      exec-once = mullvad-gui
-      exec-once = udiskie -t
-    '';
-
+      exec-once = [ "blueman-tray" "mullvad-gui" "udiskie -t" ];
+    };
   };
 }
